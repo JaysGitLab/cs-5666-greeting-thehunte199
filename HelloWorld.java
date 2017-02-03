a8add0a0422b9800fb8228727310a30a4359f91e
James Ward <wardja2@appstate.edu>
2017-02-03 10:23:51 -0500

    added optional command line arguments

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,11 @@
 public class HelloWorld {
 
   public static void main(String[] args) {
-    System.out.println("Hello Jay!");
+    String name = "yourname";
+    if (args.length > 0){
+        name = args[0];
+    }
+    System.out.println("Hello " + name);
 }
 
 }
