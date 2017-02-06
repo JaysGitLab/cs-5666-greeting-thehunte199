c36237f38cf4ef8565049853fe3008cb5f223697
James Ward <wardja2@appstate.edu>
2017-02-06 01:55:29 -0500

    conversation now functioning

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld {
 
   public static void main(String[] args) {
     String name = "yourname";
-    String compName;
+    String compName = "";
     if (args.length > 0){
         if (args.length > 1) {
             compName = args[0];
@@ -13,7 +13,12 @@ public class HelloWorld {
         }
     }
     String capName = name.substring(0, 1).toUpperCase() + name.substring(1);
-    System.out.println("Hello, " + capName + ".");
+    System.out.println("Hello, " + capName + ". ");
+    if (args.length > 1) {
+        String capComp = compName.substring(0, 1).toUpperCase() + compName.substring(1);
+        System.out.println("My name is " + capComp + ". ");
+    }
+    System.out.println("How are you doing today, " + capName + "?");
   }
 
 }
